633aef5803586601df7d698c6f9246fc178969ed
Darya Dyachkova <d.dyachkova@bk.ru>
2024-03-27 11:05:24 +0300

    .

--- Ex_10.4.3.py
+++ Ex_10.4.3.py
@@ -1,7 +1,28 @@
 import os
 
 def dir_inf(path=os.getcwd()):
-     for i in os.walk(path):
-         print(i)
+     for root, dirs, files in os.walk(path):
+         print('Текущая директория: ', root)
+         print("-" * 10)
 
-dir_inf('D:\data')
+         if dirs:
+             print("Список папок:", dirs)
+         else:
+             print('Папок нет')
+
+         if files:
+            print('Список файлов: ', files)
+         else:
+             print('Файлов нет')
+
+         if files and dirs:
+             for f in files:
+                 print('Файл', os.path.join(root, f))
+             for d in dirs:
+                 print("Папка", os.path.join(root, d))
+         print('=' * 10)
+
+
+
+
+dir_inf('D:\\new_life_new_dir\Skillfactory')
